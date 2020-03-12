40c96a07c80c8981041879c728221156f01265c8
erikrios <erikriosetiawan49@gmail.com>
2020-03-12 16:51:14 +0700

    Add data class User

---
+++ app/src/main/java/com/erikriosetiawan/mvvmarchitectureandroidbeginners/data/model/User.kt
@@ -0,0 +1,9 @@
+package com.erikriosetiawan.mvvmarchitectureandroidbeginners.data.model
+
+data class User(
+
+    val id: Int = 0,
+    val name: String = "",
+    val email: String = "",
+    val avatar: String = ""
+)
