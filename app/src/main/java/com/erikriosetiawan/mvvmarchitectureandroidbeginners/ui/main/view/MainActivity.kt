2657fd1d0240db20cf01a32078b1951df3f42fa3
erikrios <erikriosetiawan49@gmail.com>
2020-03-12 18:01:36 +0700

    Add setUpAPICall method

--- app/src/main/java/com/erikriosetiawan/mvvmarchitectureandroidbeginners/ui/main/view/MainActivity.kt
+++ app/src/main/java/com/erikriosetiawan/mvvmarchitectureandroidbeginners/ui/main/view/MainActivity.kt
@@ -2,11 +2,15 @@ package com.erikriosetiawan.mvvmarchitectureandroidbeginners.ui.main.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.erikriosetiawan.mvvmarchitectureandroidbeginners.R
 import com.erikriosetiawan.mvvmarchitectureandroidbeginners.adapter.MainAdapter
 import com.erikriosetiawan.mvvmarchitectureandroidbeginners.ui.main.viewmodel.MainViewModel
+import com.erikriosetiawan.mvvmarchitectureandroidbeginners.utils.Status
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -32,5 +36,27 @@ class MainActivity : AppCompatActivity() {
             )
         )
         recyclerView.adapter = adapter
+    }
+
+    private fun setUpAPICall() {
+        mainViewModel.getUseres().observe(this, Observer {
+            when (it.status) {
+                Status.SUCCESS -> {
+                    progressBar.visibility = View.GONE
+                    it.data?.let { users -> renderList(users) }
+                    recyclerView.visibility = View.VISIBLE
+                }
+                Status.LOADING -> {
+                    progressBar.visibility = View.VISIBLE
+                    recyclerView.visibility = View.GONE
+                }
+                Status.ERROR -> {
+                    // Handle Error
+                    progressBar.visibility = View.GONE
+                    Toast.makeText(this, it.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
+        mainViewModel.fetchUsers()
     }
 }
