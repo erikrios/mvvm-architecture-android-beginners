25f1df529b54d30e97cf2ed7c293fb1a565b20e9
erikrios <erikriosetiawan49@gmail.com>
2020-03-12 17:46:30 +0700

    Add MainAdapter class

---
+++ app/src/main/java/com/erikriosetiawan/mvvmarchitectureandroidbeginners/adapter/MainAdapter.kt
@@ -0,0 +1,41 @@
+package com.erikriosetiawan.mvvmarchitectureandroidbeginners.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.erikriosetiawan.mvvmarchitectureandroidbeginners.R
+import com.erikriosetiawan.mvvmarchitectureandroidbeginners.data.model.User
+import kotlinx.android.synthetic.main.item_layout.view.*
+
+class MainAdapter(
+    private val users: ArrayList<User>
+) : RecyclerView.Adapter<MainAdapter.DataViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataViewHolder =
+        DataViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_layout, parent, false
+            )
+        )
+
+    override fun getItemCount(): Int = users.size
+
+    override fun onBindViewHolder(holder: DataViewHolder, position: Int) =
+        holder.bind(users[position])
+
+    class DataViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(user: User) {
+            itemView.textViewUserName.text = user.name
+            itemView.textViewUserEmail.text = user.email
+            Glide.with(itemView.imageViewAvatar.context)
+                .load(user.avatar)
+                .into(itemView.imageViewAvatar)
+        }
+    }
+
+    fun addData(list: List<User>) {
+        users.addAll(list)
+    }
+}
