f5785f895c5b248abeb7c8b6e7f4cdcef680047a
erikrios <erikriosetiawan49@gmail.com>
2020-03-12 17:57:31 +0700

    Add setUpUI method

--- app/src/main/java/com/erikriosetiawan/mvvmarchitectureandroidbeginners/ui/main/view/MainActivity.kt
+++ app/src/main/java/com/erikriosetiawan/mvvmarchitectureandroidbeginners/ui/main/view/MainActivity.kt
@@ -2,12 +2,35 @@ package com.erikriosetiawan.mvvmarchitectureandroidbeginners.ui.main.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.erikriosetiawan.mvvmarchitectureandroidbeginners.R
+import com.erikriosetiawan.mvvmarchitectureandroidbeginners.adapter.MainAdapter
+import com.erikriosetiawan.mvvmarchitectureandroidbeginners.ui.main.viewmodel.MainViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainViewModel: MainViewModel
+    private lateinit var adapter: MainAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setUpUI()
+        setUpViewModel()
+        setUpAPICall()
+    }
+
+    private fun setUpUI() {
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = MainAdapter(arrayListOf())
+        recyclerView.addItemDecoration(
+            DividerItemDecoration(
+                recyclerView.context,
+                (recyclerView.layoutManager as LinearLayoutManager).orientation
+            )
+        )
+        recyclerView.adapter = adapter
     }
 }
